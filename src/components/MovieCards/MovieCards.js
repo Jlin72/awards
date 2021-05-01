42b4dbb0f57a8e2f6b49ff083d81643e89f995e0
Jhonny Lin <jhonnylin72@gmail.com>
2021-05-01 09:24:25 -0400

    removed react dnd

--- src/components/MovieCards/MovieCards.js
+++ src/components/MovieCards/MovieCards.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 // Importing components
 import DivRow from '../DivRow/DivRow';
 import Card from '../Card/Card';
 
-const MovieCards = ({movieCardAdditionalClasses, cardTitle, nomineeList, searchMovieList, buttonCb, buttonText, buttonClass, droppableId, onDragEndCb}) => {
+const MovieCards = ({movieCardAdditionalClasses, cardTitle, nomineeList, searchMovieList, buttonCb, buttonText, buttonClass}) => {
 
   const renderDisabled = (nominees, movieName) => {
     if(nominees) {
@@ -21,47 +20,17 @@ const MovieCards = ({movieCardAdditionalClasses, cardTitle, nomineeList, searchM
 
   const renderList = () => {
     if(searchMovieList) {
-      return (searchMovieList[0] === 'undefined') ? <p>No movie found</p> : 
-        searchMovieList.map((movie, index) => {
-          return (
-          <Draggable draggableId={movie.imdbID} index={index}  key={index}>
-            {(provided) => {
-              return(
-                  <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                    {movie.Title} ({movie.Year})<button className={buttonClass} disabled={renderDisabled(nomineeList, `${movie.Title} (${movie.Year})`)} id={`${movie.Title} (${movie.Year})`} onClick={buttonCb}>{buttonText}</button>
-                  </li>
-              )
-            }}
-          </Draggable>
-        )})
-
+      return (searchMovieList[0] === 'undefined') ? <p>No movie found</p> : searchMovieList.map((movie, index) => <li key={index}>{movie.Title} ({movie.Year})<button className={buttonClass} disabled={renderDisabled(nomineeList, `${movie.Title} (${movie.Year})`)} id={`${movie.Title} (${movie.Year})`} onClick={buttonCb}>{buttonText}</button></li>)
     } else if (nomineeList) {
-      return (!nomineeList.length ? <p>No movie nominated yet</p> : 
-        nomineeList.map((movie, index) => { return (
-          <Draggable draggableId={movie} index={index} key={index}>
-            {(provided) => {
-              return(
-                  <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                    {movie}<button className={buttonClass} id={movie} onClick={buttonCb}>{buttonText}</button>
-                  </li>
-              )
-            }}
-          </Draggable>
-        )}))
+      return !nomineeList.length ? <p>No movie nominated yet</p> : nomineeList.map((movie, index) => <li key={index}>{movie}<button className={buttonClass} id={movie} onClick={buttonCb}>{buttonText}</button></li>)
     }
   }
   return(
       <Card additionalClasses={`col-6 ${movieCardAdditionalClasses}`}>
         <h5 className='card-title'>{cardTitle}</h5>
-        <DragDropContext onDragEnd={onDragEndCb}>
-        <Droppable droppableId={droppableId} type='PERSON'>
-          {( provided ) => (
-            <ul {...provided.droppableProps} ref={provided.innerRef}>
-              {renderList()}
-            </ul>
-          )}
-        </Droppable>
-        </DragDropContext>
+        <ul>
+          {renderList()}
+        </ul>
       </Card>
   )
 }
