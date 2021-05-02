1dc603ae496054ba06bfc63934dab6381ef663f7
Jhonny Lin <jhonnylin72@gmail.com>
2021-05-02 17:14:05 -0400

    Added hashrouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
 import { StoreProvider } from './utils/GlobalState';
 
 import Home from './pages/Home';
 
 function App() {
   return (
-    <StoreProvider>
-      <Home />
-    </StoreProvider>
+    <HashRouter>
+      <StoreProvider>
+        <Route exact path='/' component={Home} />
+      </StoreProvider>
+    </HashRouter>
   );
 }
 
